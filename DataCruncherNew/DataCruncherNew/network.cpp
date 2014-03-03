8ebc8786cae5dbeffdee0db08e192cb369a11a4e
JoshFreemont <giosh@hotmail.co.uk>
2014-03-03 18:00:38 +0000

    rotorIdNetwork data collection 2

--- DataCruncherNew/DataCruncherNew/network.cpp
+++ DataCruncherNew/DataCruncherNew/network.cpp
@@ -16,12 +16,23 @@ network::network(const int maxNodeInit)
         edgeList.push_back(new std::vector<int>);
         isNode.push_back(new bool);
         *(isNode[i]) = false;
+        edgeFrameCreateList.push_back(new std::vector<int>);
+        
     }
+    nodeFrameCreate.reserve(maxNodeInit);
+    
 }
 
 void network::addEdge(int startNode, int endNode)
 {
-    edgeList[startNode]->push_back(endNode);
+    (*(edgeList[startNode])).push_back(endNode);
+    return;
+}
+
+//add frame of creation for child nodes to the edgeframecreatelist
+void network::addEdgeFrame(int frame, int nodeId)
+{
+    (*(edgeFrameCreateList[nodeId])).push_back(frame);
     return;
 }
 
@@ -31,10 +42,56 @@ void network::addNode(int nodeIdValue)
     return;
 }
 
+void network::addNodeFrame(int frame, int nodeId)
+{
+    nodeFrameCreate[nodeId]=frame;
+    return;
+}
+
 std::vector<std::vector<int> * > network::getEdgeList()
 {
     return edgeList;
 }
+
+std::vector<bool*> network::getIsNode()
+{
+    return isNode;
+}
+
+void network::outputEdgeList (std::ofstream& aStream)
+{
+    aStream<<"Start Node"<<"\t"<<"End Node"<<"\t"<<std::endl;
+    for(int i=0; i<maxNode; ++i)
+    {
+        if(*(isNode[i]))
+        {
+            for(auto it = (*(edgeList[i])).begin(); it != (*(edgeList[i])).end(); ++it)
+            {
+                aStream<<i<<"\t"<<*it<<"\t"<<std::endl;
+            }
+        }
+        
+        else return;
+    }
+}
+
+void network::outputTemporalEdgeList(std::ofstream& aStream)
+{
+    aStream<<"Start Node Creation Time"<<"\t"<<"End Node Creation Time"<<"\t"<<std::endl;
+    int index = 0;
+    for(auto nodeFrame = edgeFrameCreateList.begin(); nodeFrame != edgeFrameCreateList.end();  ++nodeFrame)
+    {
+        for(auto edgeFrame = (*(*nodeFrame)).begin(); edgeFrame != (*(*nodeFrame)).end(); ++edgeFrame)
+        {
+            aStream<<nodeFrameCreate[index]<<"\t"<<*edgeFrame<<"\t"<<std::endl;
+        }
+        
+        ++index;
+    }
+    
+    return;
+}
+
 
 
 
--- DataCruncherNew/DataCruncherNew/network.h
+++ DataCruncherNew/DataCruncherNew/network.h
@@ -11,19 +11,27 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 class network
 {
     int maxNode;
     std::vector<std::vector<int> * > edgeList;
     std::vector<bool*> isNode;
+    std::vector<int> nodeFrameCreate;
+    std::vector<std::vector<int> * > edgeFrameCreateList;
     
     
 public:
     network(const int maxNodeInit);//network with max node preassigned.
     void addEdge(int startNode, int endNode);
+    void addEdgeFrame(int frame, int nodeId);
     void addNode(int nodeId);
+    void addNodeFrame(int frame, int nodeId);
     std::vector<std::vector<int> * > getEdgeList();
+    std::vector<bool*> getIsNode();
+    void outputEdgeList (std::ofstream& aStream);
+    void outputTemporalEdgeList (std::ofstream& aStream);
 };
 
 

c2f4468720406e5f30d2d05aa7ca9299e32d5000
JoshFreemont <giosh@hotmail.co.uk>
2014-03-07 14:48:05 +0000

    cleanup1

--- DataCruncherNew/DataCruncherNew/main_functions.h
+++ DataCruncherNew/DataCruncherNew/main_functions.h
@@ -13,10 +13,9 @@
 #include "array2D.h"
 #include <utility>
 #include <string>
+#include "rotorIDstruct.h"
 
-
-
-
+//Inline Update Functions.
 inline void update_arrays(int &state_update, std::vector<int> &all_excited_coords_cyclic, int &excitation_frame_update,  const int& frame, const int& RP, const int& i_coord, const int& j_coord)
 {
     state_update=RP;
@@ -26,10 +25,29 @@ inline void update_arrays(int &state_update, std::vector<int> &all_excited_coord
     return;
 }
 
-const std::string currentDateTime() ;
+inline void updateRotorIdData(rotorIDstruct& rotor, const int& cycleLength, const int& averageX, const int& averageY)
+{
+    rotor.lifetime++;
+    rotor.length.push_back(cycleLength);
+    rotor.deathX = averageX;
+    rotor.deathY = averageY;
+    return;
+}
 
+inline void updateRotorCoords(std::vector<int> rotorCoordsNow, int& i, int& j, int& state, int& Id)
+{
+    rotorCoordsNow.push_back(i);
+    rotorCoordsNow.push_back(j);
+    rotorCoordsNow.push_back(state);
+    rotorCoordsNow.push_back(Id);
+}
+
+
+//Pacemaker Function
 void pacemaker(array2D<int> &state_update, std::vector<int> &all_excited_coords, array2D<int> &excitation_frame, const int &frame, const int &RP, const int& GRIDSIZE, array2D<std::pair<int,int>> &excitedBy);
 
-void init_Kishan(double*** inN, double*** inE, double*** inS, double*** inW, const int GRIDSIZE, double defect_density, double vert_conn, double firing_prob);
+//CurrentDatetime function
+const std::string currentDateTime() ;
+
 
 #endif /* defined(__sdltest__main_functions__) */

8b608e0793e8bbbde292385a2bc04defc332b533
Basil Lim <BHB_Lim@yahoo.com>
2014-03-11 12:01:13 +0000

    standarddev function should work now

--- DataCruncherNew/DataCruncherNew/analysisfunctions.h
+++ DataCruncherNew/DataCruncherNew/analysisfunctions.h
@@ -8,7 +8,7 @@
 
 double mean(std::vector<int>& someVector);
 double mean(std::vector<double>& someVector); //overloaded just in case
-double stddev(std::vector<int>& someVector, double mean);
-double stddev(std::vector<double>& someVector, double mean);
+double standarddev(std::vector<int>& someVector, double mean);
+double standarddev(std::vector<double>& someVector, double mean);
 
 #endif //__ANALYSISFUNCTIONS__H__

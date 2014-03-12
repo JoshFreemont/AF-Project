efca275cfcffde4a743c639897da5d3ffae8df73
JoshFreemont <giosh@hotmail.co.uk>
2014-03-12 12:58:59 +0000

    rounding adjustment

--- DataCruncherNew/DataCruncherNew/rotorFunctions.h
+++ DataCruncherNew/DataCruncherNew/rotorFunctions.h
@@ -84,8 +84,8 @@ inline void calcAvPos(const int& cycleStart, const int& cycleLength, std::pair<i
     
     avX /= (double)(cycleLength);
     avY /= (double)(cycleLength);
-	avX = floor(avX+0.5);
-    avY = (double)(static_cast<int>((floor(avY+0.5)+GRIDSIZE))%GRIDSIZE);//enforce correct bounds.
+	avX = round(avX);
+    avY = (double)(static_cast<int>(round(avY+GRIDSIZE))%GRIDSIZE);//enforce correct bounds.
     
     return;
 }
